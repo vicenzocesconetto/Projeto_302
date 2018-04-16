f03923ed8ef9e983f4c5dcd6bcf3ddb150a40ad2
vicenzo-cesconetto <vicenzoccesconetto@gmail.com>
2018-04-16 15:46:52 -0300

    started the class 'Empresa'

---
+++ Empresa.java
@@ -0,0 +1,13 @@
+/*
+Empresa: - Dados da empresa - Caixa - ArryList Movimentação
+*/
+
+import java.util.ArryList;
+
+public class Empresa{
+    private String nome;
+    private String cnpj;
+    private float caixa;
+    private A
+
+}
