c7e521b32b23a86be79c3b94c85d228ec6df3d7b
vicenzo-cesconetto <vicenzoccesconetto@gmail.com>
2018-04-16 16:15:51 -0300

    Continuing Empresa.java

--- Empresa.java
+++ Empresa.java
@@ -1,13 +1,51 @@
-/*
-Empresa: - Dados da empresa - Caixa - ArryList Movimentação
-*/
-
-import java.util.ArryList;
+import java.util.ArrayList;
 
 public class Empresa{
     private String nome;
     private String cnpj;
     private float caixa;
-    private A
+    private ArrayList<String> movimentacao;
 
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public float getCaixa() {
+        return caixa;
+    }
+
+    public void setCaixa(float caixa) {
+        this.caixa = caixa;
+    }
+
+    public ArrayList<String> getMovimentacao() {
+        return movimentacao;
+    }
+
+    public void setMovimentacao(ArrayList<String> movimentacao) {
+        this.movimentacao = movimentacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa{" +
+                "nome='" + nome + '\'' +
+                ", cnpj='" + cnpj + '\'' +
+                ", caixa=" + caixa +
+                ", movimentacao=" + movimentacao +
+                '}';
+    }
 }
