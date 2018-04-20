12620f11d6eb1c1873a603f90605f97772a4c8e3
Vicenzo Cano Cesconetto <ra206456@felix.lab.ic.unicamp.br>
2018-04-20 14:32:54 -0300

    improve Main class

--- Main.java
+++ Main.java
@@ -1,12 +1,27 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main{
   private ArrayList<Cliente> clientes;
-  ArrayList<Produto> produtos;
-  ArrayList<Servico> servicos;
+  private ArrayList<Produto> produtos;
+  private ArrayList<Servico> servicos;
   private Empresa empresa;
 
   public static void main(String[] args) {
+    int comandoDoUsuario;
+    Scanner usuarioInput = new Scanner(System.in);
 
-  }
+    System.out.println("O que voce deseja fazer?");
+    System.out.println("[1] Buscar cliente, produtos ou servicos");
+    System.out.println("[2] Adicionar cliente, produtos ou servicos");
+    System.out.println("[3] Atualizar estoque");
+
+    comandoDoUsuario = usuarioInput.NextLine();
+
+    switch (comandoDoUsuario) {
+      case 1:
+      case 2:
+      case 3:
+    }
+  }  
 }
