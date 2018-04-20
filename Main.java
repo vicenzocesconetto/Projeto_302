0481f3e904e7d20d4f97097faa813a2013d23959
Vicenzo Cano Cesconetto <ra206456@felix.lab.ic.unicamp.br>
2018-04-20 14:16:28 -0300

    add Main class

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main{
+  private ArrayList<Cliente> clientes;
+  ArrayList<Produto> produtos;
+  ArrayList<Servico> servicos;
+  private Empresa empresa;
+
+  public static void main(String[] args) {
+
+  }
+}
