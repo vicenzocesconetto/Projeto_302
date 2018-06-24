63253b04858f27ff46e136901c1cd1e134402729
vicenzo-cesconetto <vicenzoccesconetto@gmail.com>
2018-06-24 11:10:15 -0300

    adicionar arraylist de veiculos em cliente

--- Cliente.java
+++ Cliente.java
@@ -3,19 +3,24 @@ import java.util.ArrayList;
 
 public class Cliente implements Serializable{
 	private static final long serialVersionUID = 1L;
-	private static int numClientes=0;
+	private static int numClientes;
 	private String nome;
 	private String telefone;
 	private String email;
 	private Endereco endereco;
-	
+	private ArrayList<Veiculo> veiculos;
 	private ArrayList<Venda> historico;
+
+	static {
+        numClientes = 0;
+    }
 	
 	public Cliente(String nome, String telefone, String email, Endereco endereco) {
 		this.nome = nome.toUpperCase();
 		this.telefone = telefone.toUpperCase();
 		this.email = email.toLowerCase();
 		this.endereco = endereco;
+		this.veiculos = new ArrayList<Veiculo>();
 		this.historico = new ArrayList<Venda>();
 		++numClientes;
 	}
