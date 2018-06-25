d541241e893ec0312617ca67c83be3bc5332a563
vicenzo-cesconetto <vicenzoccesconetto@gmail.com>
2018-06-24 22:37:22 -0300

    adiciona construtor com mensagem em EstoqueInsuficienteException

--- EstoqueInsuficienteException.java
+++ EstoqueInsuficienteException.java
@@ -1,2 +1,8 @@
 public class EstoqueInsuficienteException extends Exception {
+
+    public EstoqueInsuficienteException() {}
+
+    public EstoqueInsuficienteException(String message) {
+        super(message);
+    }
 }
